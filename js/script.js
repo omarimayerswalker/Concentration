ec271b0440d697be701180856ee7421fb7b42055
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-13 19:17:26 -0400

    added images

--- js/script.js
+++ js/script.js
@@ -1,38 +1,48 @@
     // Player Moves
 
-var playerMoves = {
+    var playerMoves = {
         twoCards: [],
-        cardSelect: 
-    $('.card').on('click', function(event) {
-        event.preventDefault()
-        console.log("Card Select")
-    }),
+        cardCounter: null,
+        cardSelect: $('.card').on('click', function(event) {
+            event.preventDefault()
+            playerMoves.twoCards.push(event.target.id)
+            playerMoves.reveal(event.target)
+        }),
         cardHighlight:
         //Card Highlighter
-            $('.card').on('mouseover', function(event) {
-            event.preventDefault()
-                console.log("Card Highlighter")
-                // selected cards are placed in an array
-                // if statement will prevent player from selecting
-                // more than 2 cards
-        })
+            $('.card').hover(function() {
+            $(this).addClass('highlight')
+        }, function() {
+            $(this).removeClass('highlight')
+        }),
+        reveal: function(event) {
+                console.log(event)
+                $(event).css('background-color', 'blue')
+                    // for (var i = 0; i < 2; i++) {
+                    // $(this).css('background-color', 'red')
+
+                // }
+            }
+            // Reveal the reverse of the selected card
     }
+
+    var cardDeck1 = {}
 
     // Buttons
 
-var buttons = {
-    reset:
-    // Reset button: complete
-        $('#reset').on('click', function(event) {
-        event.preventDefault()
-        window.location.reload()
-    }),
+    var buttons = {
+        reset:
+        // Reset button: complete
+            $('#reset').on('click', function(event) {
+            event.preventDefault()
+            window.location.reload()
+        }),
 
-    play:
-    // Play button
-        $('#play').on('click', function(event) {
-        event.preventDefault()
+        play:
+        // Play button
+            $('#play').on('click', function(event) {
+            event.preventDefault()
             console.log("Play Button")
-            // trigger concentration()
-    })
-}
+                // trigger concentration()
+        })
+    }
