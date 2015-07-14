e530a89843a758e086958e962781fbff360373c1
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-14 14:07:21 -0400

    redesigning arrangeBoard()

--- js/script.js
+++ js/script.js
@@ -28,70 +28,128 @@
 
     // Store card fronts
     var gamePlay = {
-        // Play button triggers alert to play
-        start: function() {
-            gamePlay.shuffle()
-            alert("Select two cards untill you've matched them all!")
-        },
+            // Play button triggers alert to play
+            start: function() {
+                gamePlay.arrangeBoard()
+                alert("Select two cards untill you've matched them all!")
+            },
 
-        // Card Deck
+            // Card Deck
 
-        cardDeck: ["android",
-            "angular",
-            "antergos",
-            "arch",
-            "backbone",
-            "bitcoin",
-            "bower",
-            "C",
-            "C++",
-            "chakra",
-            "clementine",
-            "coffee",
-            "copyleft",
-            "csharp",
-            "css3",
-            "dart",
-            "debian",
-            "diaspora",
-            "dj",
-            "django",
-            "docker",
-            "dogecoin",
-            "drupal",
-            "elementary",
-            "emacs",
-            "fluxbox",
-            "freebsd",
-            "gentoo",
-            "gimp",
-            "git",
-            "gnome",
-            "gnu",
-            "golang",
-            "grunt",
-            "html5",
-            "js"
-                  ],
-        // Taken cards
-        taken: [],
+            cardDeck1: ["android",
+                "angular",
+                "antergos",
+                "arch",
+                "backbone",
+                "bitcoin",
+                "bower",
+                "C",
+                "C++",
+                "chakra",
+                "clementine",
+                "coffee",
+                "copyleft",
+                "csharp",
+                "css3",
+                "dart"
+            ],
 
-        // Randomize cards
-        shuffle: function() {
-            // places random cards onto the board with display hidden
-            console.log("random")
-            // Selects random Card
-            while (gamePlay.taken.length < 18){
-            var randomCard = gamePlay.cardDeck[Math.floor(Math.random() * gamePlay.cardDeck.length)];
-            gamePlay.taken.push(randomCard)
-                for(i in gamePlay.taken){
-                    if (i == randomCard){
-                        gamePlay.taken.pop(i)
-                    }
-                        }
-            }
-            console.log(gamePlay.taken)
-        }
+            cardDeck2: [
+                "debian",
+                "diaspora",
+                "dj",
+                "django",
+                "docker",
+                "dogecoin",
+                "drupal",
+                "elementary",
+                "emacs",
+                "fluxbox",
+                "freebsd",
+                "gentoo",
+                "gimp",
+                "git",
+                "gnome",
+                "gnu",
+                "golang",
+                "grunt",
+                "html5",
+                "js"
+            ],
+            // Taken cards
+            taken: [],
+
+            // Card positions in DOM
+
+            boardPosition: ["r1c1", // 0
+                "r1c2", //  1
+                "r1c3", //  2
+                "r1c4", //  3
+                "r1c5", //  4
+                "r1c6", //  5
+                "r1c7", //  6
+                "r1c8", //  7
+                "r1c9", //  8
+
+                "r2c1", //  9
+                "r2c2", // 10
+                "r2c3", // 11
+                "r2c4", // 12
+                "r2c5", // 13
+                "r2c6", // 14
+                "r2c7", // 15
+                "r2c8", // 16
+                "r2c9", // 17
+
+                "r3c1", // 18
+                "r3c2", // 19
+                "r3c3", // 20
+                "r3c4", // 21
+                "r3c5", // 22
+                "r3c6", // 23
+                "r3c7", // 24
+                "r3c8", // 25
+                "r3c9", // 26
+
+                "r3c1", // 27
+                "r3c2", // 28
+                "r3c3", // 29
+                "r3c4", // 30
+                "r3c5", // 31
+                "r3c6", // 32
+                "r3c7", // 33
+                "r3c8", // 34
+                "r3c9", // 35
+            ],
+
+            boardTop: [],
+            fullBoard: []
+
+            // shuffle: function(o) {
+            // for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+            // return gamePlay.fullBoard
+            // },
+
+            // // Randomize cards
+            // arrangeBoard: function() {
+            //     // places random cards onto the board with display hidden
+            //     // console.log("random")
+            //     // Selects random Card
+            //     while (gamePlay.taken.length < 18) {
+            //         var randomCard = gamePlay.cardDeck[Math.floor(Math.random() * gamePlay.cardDeck.length)];
+            //         gamePlay.taken.push(randomCard)
+            //             // remove duplicates
+            //         $.each(gamePlay.taken, function(i, el) {
+            //             if ($.inArray(el, gamePlay.boardTop) === -1) gamePlay.bo
+            //         })
+
+        // }
+        // gamePlay.boardTop = gamePlay.taken
+        // gamePlay.fullBoard = gamePlay.taken
+        // console.log(gamePlay.boardTop)
+        // gamePlay.shuffle(gamePlay.fullBoard)
+        // }
+
     }
 
     // Buttons
@@ -108,7 +166,7 @@
         // Play button
             $('#play').on('click', function(event) {
             event.preventDefault()
-            console.log("Play Button")
+                // console.log("Play Button")
             gamePlay.start();
             // trigger concentration()
         })
