cc48dac2e26d640cd5bbe1055918c3c259107ab0
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-14 22:05:00 -0400

    completed reveal()

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@
             playerMoves.twoCards.push(event.target.id)
             playerMoves.reveal(event.target)
         }),
+
         cardHighlight:
         //Card Highlighter
             $('.card').hover(function() {
@@ -16,13 +17,12 @@
             $(this).removeClass('highlight')
         }),
         reveal: function(event) {
+            var clickedCard = $(event).html()
             // Reveal the reverse of the selected card
             console.log(event)
-            $(event).css('background', 'none')
-                // for (var i = 0; i < 2; i++) {
-                // $(this).css('background-color', 'red')
-
-            // }
+            console.log(clickedCard)
+            // $(event).removeClass('back')
+            $(event).addClass(clickedCard)
         }
     }
 
@@ -56,25 +56,6 @@
             "diaspora"
         ],
 
-        cardDeck2: ["dj",
-            "django",
-            "docker",
-            "dogecoin",
-            "drupal",
-            "elementary",
-            "emacs",
-            "fluxbox",
-            "freebsd",
-            "gentoo",
-            "gimp",
-            "git",
-            "gnome",
-            "gnu",
-            "golang",
-            "grunt",
-            "html5",
-            "js"
-        ],
         // Taken cards
         taken: [],
 
@@ -110,39 +91,84 @@
             "r3c8", // 25
             "r3c9", // 26
 
-            "r3c1", // 27
-            "r3c2", // 28
-            "r3c3", // 29
-            "r3c4", // 30
-            "r3c5", // 31
-            "r3c6", // 32
-            "r3c7", // 33
-            "r3c8", // 34
-            "r3c9", // 35
+            "r4c1", // 27
+            "r4c2", // 28
+            "r4c3", // 29
+            "r4c4", // 30
+            "r4c5", // 31
+            "r4c6", // 32
+            "r4c7", // 33
+            "r4c8", // 34
+            "r4c9", // 35
         ],
 
         fullBoard: [],
         shuffledBoard: [],
 
+        // Creates 2 instances of each card used
         arrangeBoard: function() {
             gamePlay.fullBoard = gamePlay.cardDeck1.concat(gamePlay.cardDeck1)
-            console.log(gamePlay.fullBoard)
+                // console.log(gamePlay.fullBoard)
             return gamePlay.shuffle(gamePlay.fullBoard)
-            // console.log(gamePlay.fullBoard)
+                // console.log(gamePlay.fullBoard)
         },
 
+        // Shuffles cards
         shuffle: function(o) {
             for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x)
-            gamePlay.shuffledBoard = o
+                gamePlay.shuffledBoard = o
             console.log(gamePlay.shuffledBoard)
-            return gamePlay.shuffledBoard
-        }
+            return gamePlay.addToBoard()
+        },
 
+        // Associates the cards to the DOM
+        addToBoard: function() {
+                $("#r1c1").append(gamePlay.shuffledBoard[0])
+                $("#r1c2").append(gamePlay.shuffledBoard[1])
+                $("#r1c3").append(gamePlay.shuffledBoard[2])
+                $("#r1c4").append(gamePlay.shuffledBoard[3])
+                $("#r1c5").append(gamePlay.shuffledBoard[4])
+                $("#r1c6").append(gamePlay.shuffledBoard[5])
+                $("#r1c7").append(gamePlay.shuffledBoard[6])
+                $("#r1c8").append(gamePlay.shuffledBoard[7])
+                $("#r1c9").append(gamePlay.shuffledBoard[8])
+
+                $("#r2c1").append(gamePlay.shuffledBoard[9])
+                $("#r2c2").append(gamePlay.shuffledBoard[10])
+                $("#r2c3").append(gamePlay.shuffledBoard[11])
+                $("#r2c4").append(gamePlay.shuffledBoard[12])
+                $("#r2c5").append(gamePlay.shuffledBoard[13])
+                $("#r2c6").append(gamePlay.shuffledBoard[14])
+                $("#r2c7").append(gamePlay.shuffledBoard[15])
+                $("#r2c8").append(gamePlay.shuffledBoard[16])
+                $("#r2c9").append(gamePlay.shuffledBoard[17])
+
+                $("#r3c1").append(gamePlay.shuffledBoard[18])
+                $("#r3c2").append(gamePlay.shuffledBoard[19])
+                $("#r3c3").append(gamePlay.shuffledBoard[20])
+                $("#r3c4").append(gamePlay.shuffledBoard[21])
+                $("#r3c5").append(gamePlay.shuffledBoard[22])
+                $("#r3c6").append(gamePlay.shuffledBoard[23])
+                $("#r3c7").append(gamePlay.shuffledBoard[24])
+                $("#r3c8").append(gamePlay.shuffledBoard[25])
+                $("#r3c9").append(gamePlay.shuffledBoard[26])
+
+                $("#r4c1").append(gamePlay.shuffledBoard[27])
+                $("#r4c2").append(gamePlay.shuffledBoard[28])
+                $("#r4c3").append(gamePlay.shuffledBoard[29])
+                $("#r4c4").append(gamePlay.shuffledBoard[30])
+                $("#r4c5").append(gamePlay.shuffledBoard[31])
+                $("#r4c6").append(gamePlay.shuffledBoard[32])
+                $("#r4c7").append(gamePlay.shuffledBoard[33])
+                $("#r4c8").append(gamePlay.shuffledBoard[34])
+                $("#r4c9").append(gamePlay.shuffledBoard[35])
+        }
     }
 
     // Buttons
 
     var buttons = {
+        // Reset button
         reset:
         // Reset button: complete
             $('#reset').on('click', function(event) {
@@ -150,6 +176,7 @@
             window.location.reload()
         }),
 
+        // Play button
         play:
         // Play button
             $('#play').on('click', function(event) {
